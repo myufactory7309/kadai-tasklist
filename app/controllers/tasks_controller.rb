f0fb84a178b07be42b58d12e9dc14b326a29b01d
myufactory <myufactory7309@gmail.com>
2018-12-07 07:30:22 +0000

    kadai-tasklist add login 4th?

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,28 +1,35 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in, only:[:index, :new, :create, :edit, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   before_action :set_task, only:[:show, :edit, :update, :destory]
-  before_action :require_user_logged_in
-  before_action :correct_user, only: [:new, :show, :edit, :update, :destroy]
+
   
   #タスク全部を代入するから名前も複数になっている!?
   def index
-    @tasks = current_user.tasks.order(created_at: :desc).page(params[:page]).per(5)
-  end 
-  
-  #１個のタスクを代入するから名前も単数になっている!?
-  def show
-    @task = Task.find(params[:id])
+    # @task = current_user.tasks.build  # form_for 用
+    @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
   end 
   
   def new
     @task = Task.new
   end
   
+  #１個のタスクを代入するから名前も単数になっている!?
+  def show
+    @task = Task.find(params[:id])
+  end 
+  
+
+  
   def create
-    @task = Task.new(task_params)
+    #@task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = 'タスクが正常に投稿されました'
-      redirect_to @task
+      #redirect_to @task
+      redirect_to root_url
+
     else
       flash.now[:danger] = 'タスクが投稿されませんでした'
       render :new
@@ -30,10 +37,10 @@ class TasksController < ApplicationController
   end
   
 
-  
   def edit
     set_task
   end 
+  
   
   def update
     set_task
@@ -46,6 +53,7 @@ class TasksController < ApplicationController
       render :edit
     end
   end 
+  
   
   def destroy
     set_task
@@ -65,7 +73,7 @@ class TasksController < ApplicationController
   end
   
   def correct_user
-    @task = current_user.task.find_by(id: params[:id])
+    @task = current_user.tasks.find_by(id: params[:id])
     unless @task
       redirect_to root_url
     end
@@ -75,9 +83,6 @@ class TasksController < ApplicationController
     params.require(:task).permit(:content, :status)
   end
   
-  
-    
-
   
 end
 
